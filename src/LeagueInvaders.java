07771c20d492afe064c77ae960049ce886132c1f
League Student <league@iMac-16.attlocal.net>
2020-01-12 12:57:35 -0800

    league invaders ip

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,20 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	public static final int WIDTH=500;
+	public static final int HEIGHT = 800;
+	
+	JFrame frame = new JFrame();
+public static void main(String[] args) {
+	
+}
+LeagueInvaders(){
+	
+}
+void setup() {
+	frame.setSize(WIDTH,HEIGHT);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 
 }
