69ddce5ea1688db99ea61ca0504ab3cab67efc4b
root <root@bc70c4cdc516>
2020-03-26 02:57:16 +0000

    leage invaders Model Manegement step three

--- src/Alien.java
+++ src/Alien.java
@@ -1,6 +1,16 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Alien extends GamePanel {
+public class Alien extends GameObject {
 	Alien(int x, int y, int width, int height) {
-		
+		super(x, y, width, height);
+		speed=1;
 }
+     void draw (Graphics g){
+    	 g.setColor(Color.yellow);
+    	 g.fillRect(x,y,width,height);
+  }
+	void update() {
+	y+=speed;	
+	}
 }
---
+++ src/ObjectManager.java
@@ -0,0 +1,8 @@
+
+public class ObjectManager {
+Rocketship r;
+
+ObjectManager(Rocketship r){
+	this.r=r;
+}
+}
