4bdd5225f284529f39c160bfadf264bc92a7fa12
root <root@2a081a2fdeac>
2020-03-29 19:59:41 +0000

    leagueinvaders

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
 
 public class ObjectManager {
 Rocketship r;
+ArrayList<Projectile> projectiles=new ArrayList<Projectile>();
 
 ObjectManager(Rocketship r){
 	this.r=r;
+
+}
+void addProjectile() {
+	
 }
 }
